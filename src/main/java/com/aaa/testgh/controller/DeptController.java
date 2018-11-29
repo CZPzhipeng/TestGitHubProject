d322a40c717381429277d22aa13ace3132448b10
志鹏 <1255983308@qq.com>
2018-11-29 09:40:17 +0800

    新加了EmpController

---
+++ src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -0,0 +1,16 @@
+package com.aaa.testgh.controller;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * className:DeptController
+ * discription:
+ * author:CZP
+ * createTime:2018-11-29 09:08
+ */
+public class DeptController {
+
+    public static void main(String[] args) {
+        System.out.println("测试gethub 提交111111");
+    }
+}
